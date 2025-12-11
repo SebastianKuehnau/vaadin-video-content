efa829d7e62f1a60a9a4aa487f39d0297bf8df95
Sebastian Kühnau <seb@vaadin.com>
2025-12-11 11:12:42 +0100

    Refactor `ProductsView` to use new scoped signals and implement local factor UI; added styles for local factor field.

--- 003-simple-signals/src/main/java/com/example/vaadin/views/ProductsView.java
+++ 003-simple-signals/src/main/java/com/example/vaadin/views/ProductsView.java
@@ -4,67 +4,84 @@ import com.example.vaadin.data.Product;
 import com.example.vaadin.data.ProductRepository;
 import com.vaadin.flow.component.ComponentEffect;
 import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.NumberField;
+import com.vaadin.flow.component.textfield.TextFieldVariant;
 import com.vaadin.flow.data.renderer.ComponentRenderer;
 import com.vaadin.flow.router.Route;
 import com.vaadin.signals.NumberSignal;
-import com.vaadin.signals.SignalFactory;
+import org.jspecify.annotations.NonNull;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Route("")
 public class ProductsView extends VerticalLayout {
 
     // Signal for VAT Factor for multiple clients
-    private final NumberSignal vatSignal = SignalFactory.IN_MEMORY_SHARED.number("vat", 19.0);
+    private static final NumberSignal globalSignal = new NumberSignal(42.0);
 
-    // Signal for local multiply factor
-    private final NumberSignal multiplySignal = new NumberSignal(1.0);
+    // Signal for UI Scoped multiply Factor
+    private final NumberSignal uiScopedSignal = new NumberSignal(0.5);
+
+    // Signal for Local multiply Factor
+    private final Map<String, NumberSignal> localSignalMap = new ConcurrentHashMap<>();
 
     public ProductsView(ProductRepository repository) {
 
-        var vatField = new NumberField("VAT");
-        vatField.setStep(0.5);
-        vatField.setStepButtonsVisible(true);
-        vatField.setSuffixComponent(new Div("%"));
-        vatField.addValueChangeListener(event -> {
-            if (event.isFromClient()) //to avoid changes in a read-only transaction
-                vatSignal.value(event.getValue());
-        });
-        ComponentEffect.bind(vatField, vatSignal, NumberField::setValue);
-
-        var multiplyField = new NumberField("Multiply");
-        multiplyField.setStep(1.0);
-        multiplyField.setStepButtonsVisible(true);
-        multiplyField.addValueChangeListener(event -> multiplySignal.value(event.getValue()));
-        multiplyField.setValue(multiplySignal.value());
+        var vatField = createNumberField("Global Factor", globalSignal);
+        var multiplyField = createNumberField("UI Scoped Factor", uiScopedSignal);
 
         add(new HorizontalLayout(vatField, multiplyField));
 
         var grid = new Grid<>(Product.class);
-        grid.setColumns("title", "content");
-        grid.addColumn("price").setHeader("Net Price");
-        grid.addColumn(new ComponentRenderer<>(article -> {
-            Span priceSpan = new Span();
+        grid.setColumns("title", "content", "price");
+        grid.addColumn(new ComponentRenderer<>(product -> {
+            var localSignal = localSignalMap.computeIfAbsent("localFactor/" + product.getId(), k -> new NumberSignal(1.2));
 
-            // Effect for showing the current price with VAT
+            var localFactorField = createNumberField("", localSignal);
+            localFactorField.addThemeVariants(TextFieldVariant.LUMO_SMALL);
+            localFactorField.addClassName("local-factor-field");
+            return localFactorField;
+        })).setHeader("Local Factor").setAutoWidth(true);
+
+        grid.addColumn(new ComponentRenderer<>(product -> {
+            var localSignal = localSignalMap.computeIfAbsent("localFactor/" + product.getId(), k -> new NumberSignal(1.2));
+
+            var priceSpan = new Span();
+
             ComponentEffect.effect(priceSpan, () ->
-                    priceSpan.setText(article.getPrice()
-                            .multiply(BigDecimal.ONE.add(BigDecimal.valueOf(vatSignal.value() / 100)))
-                            .multiply(BigDecimal.valueOf(multiplySignal.value()))
-                            .setScale(2, RoundingMode.HALF_UP).toString())
-            );
+                    priceSpan.setText(
+                            product.getPrice()
+                                    .multiply(BigDecimal.valueOf(globalSignal.value()))
+                                    .multiply(BigDecimal.valueOf(uiScopedSignal.value()))
+                                    .multiply(BigDecimal.valueOf(localSignal.value()))
+                                    .setScale(2, RoundingMode.HALF_UP)
+                                    .toString()));
 
             return priceSpan;
-        })).setHeader("Price with VAT");
+        })).setHeader("Calculated Price").setAutoWidth(true);
 
         grid.setItems(repository.findAll());
 
         add(grid);
+    }
+
+    private static @NonNull NumberField createNumberField(String Global_Factor, NumberSignal signal) {
+        var numberField = new NumberField(Global_Factor);
+        //<theme-editor-local-classname>
+        numberField.setStep(0.1);
+        numberField.setStepButtonsVisible(true);
+        numberField.addValueChangeListener(event -> {
+            if (event.isFromClient()) //to avoid changes in a read-only transaction
+                signal.value(event.getValue());
+        });
+        ComponentEffect.bind(numberField, signal, NumberField::setValue);
+
+        return numberField;
     }
 }
