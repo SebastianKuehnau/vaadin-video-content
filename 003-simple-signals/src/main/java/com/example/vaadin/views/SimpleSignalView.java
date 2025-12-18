229ff677a398507dbc2d0884116d7feb76d435b6
Sebastian Kühnau <seb@vaadin.com>
2025-12-18 18:33:36 +0100

    Refactor `SimpleSignalView` to use a shared static `NumberSignal` for improved state management and simplify badge theme binding logic.

--- 003-simple-signals/src/main/java/com/example/vaadin/views/SimpleSignalView.java
+++ 003-simple-signals/src/main/java/com/example/vaadin/views/SimpleSignalView.java
@@ -14,6 +14,12 @@ public class SimpleSignalView extends VerticalLayout {
     public static final String BADGE_CONTRAST = "badge contrast";
     public static final String BADGE_PRIMARY_SUCCESS = "badge primary success";
 
+    /***
+     * 6. By declaring the signal as a static field, you create a shared state: This allows other users or tabs to
+     * react to changes in real time — without any extra implementation effort.
+     */
+    private final static NumberSignal numberSignal = new NumberSignal(42.0);
+
     public SimpleSignalView() {
 
         /***
@@ -26,38 +32,26 @@ public class SimpleSignalView extends VerticalLayout {
          * signal, ensuring that any value changes propagate automatically to the rest of the UI.
          */
 
-//        var numberSignal = new NumberSignal(42.0);
+        //var numberSignal = new NumberSignal(42.0);
 
         /***
          * 2. Take this scenario: We have a number field and two badges for 'even' and 'odd'. Depending on the input,
          * the corresponding badge reactively turns green to indicate the current state.
          */
         var numberField = new NumberField("Local Number");
-        numberField.setStep(1);
         numberField.setStepButtonsVisible(true);
+        numberField.addValueChangeListener(e -> numberSignal.value(e.getValue()));
+        ComponentEffect.bind(numberField, numberSignal, NumberField::setValue);
 
-//        numberField.addValueChangeListener(e -> numberSignal.value(e.getValue()));
-//        ComponentEffect.bind(numberField, numberSignal, NumberField::setValue);
-
-        var evenBadge = new Span("even");
         /***
          * 5. Signals eliminate the need for complex, cross-component listeners. By letting each component react to the
          * state independently, we decouple our UI and simplify the overall architecture.
          */
-//        evenBadge.getElement().bindAttribute("theme", () -> numberSignal.value() % 2 == 0 ? BADGE_CONTRAST : BADGE_PRIMARY_SUCCESS);
-//        ComponentEffect.effect(evenBadge, () -> {
-//            evenBadge.getElement().getThemeList().clear();
-//            evenBadge.getElement().getThemeList().add(numberSignal.value() % 2 == 0 ?
-//                    BADGE_PRIMARY_SUCCESS : BADGE_CONTRAST);
-//        });
+        var evenBadge = new Span("even");
+        evenBadge.getElement().bindAttribute("theme", () -> numberSignal.value() % 2 == 0 ? BADGE_CONTRAST : BADGE_PRIMARY_SUCCESS);
 
         var unevenBadge = new Span("uneven");
-//        unevenBadge.getElement().bindAttribute("theme", () -> numberSignal.value() % 2 == 0 ? BADGE_PRIMARY_SUCCESS : BADGE_CONTRAST);
-//        ComponentEffect.effect(unevenBadge, () -> {
-//            unevenBadge.getElement().getThemeList().clear();
-//            unevenBadge.getElement().getThemeList().add(numberSignal.value() % 2 == 0 ?
-//                    BADGE_CONTRAST : BADGE_PRIMARY_SUCCESS);
-//        });
+        unevenBadge.getElement().bindAttribute("theme", () -> numberSignal.value() % 2 == 0 ? BADGE_PRIMARY_SUCCESS : BADGE_CONTRAST);
 
 
         /***
@@ -65,17 +59,17 @@ public class SimpleSignalView extends VerticalLayout {
          * coupling between the components. The input field ends up managing the state of its neighbors, making the
          * code harder to maintain.
          */
-        numberField.addValueChangeListener(event -> {
-            boolean isEven = event.getValue().intValue() % 2 == 0;
-
-            evenBadge.getElement().getThemeList().clear();
-            unevenBadge.getElement().getThemeList().clear();
-
-            evenBadge.getElement().getThemeList().add(isEven ? BADGE_PRIMARY_SUCCESS : BADGE_CONTRAST);
-            unevenBadge.getElement().getThemeList().add(isEven ? BADGE_CONTRAST : BADGE_PRIMARY_SUCCESS);
-        });
-
-        numberField.setValue(0.0);
+//        numberField.addValueChangeListener(event -> {
+//            boolean isEven = event.getValue().intValue() % 2 == 0;
+//
+//            evenBadge.getElement().getThemeList().clear();
+//            unevenBadge.getElement().getThemeList().clear();
+//
+//            evenBadge.getElement().getThemeList().add(isEven ? BADGE_PRIMARY_SUCCESS : BADGE_CONTRAST);
+//            unevenBadge.getElement().getThemeList().add(isEven ? BADGE_CONTRAST : BADGE_PRIMARY_SUCCESS);
+//        });
+//
+//        numberField.setValue(0.0);
         add(numberField , new HorizontalLayout(evenBadge, unevenBadge));
     }
 }
